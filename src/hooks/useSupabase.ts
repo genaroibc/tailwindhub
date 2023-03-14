418d32b8aa93854f2135d3d75ac3bf0cf15112e9
genaroibc <bonavitaismael@gmail.com>
2023-03-14 18:49:41 -0300

    [DEV]: add useSupabase hook

---
+++ src/hooks/useSupabase.ts
@@ -0,0 +1,14 @@
+import { SupabaseContext } from "@/context/SupabaseContext";
+import { useContext } from "react";
+
+export function useSupabase() {
+  const context = useContext(SupabaseContext);
+
+  if (context === undefined) {
+    throw new Error(
+      "'useSupabase' hook must be used inside a SupabaseProvider"
+    );
+  }
+
+  return context;
+}
