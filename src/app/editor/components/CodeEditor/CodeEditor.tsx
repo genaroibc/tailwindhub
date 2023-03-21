8c6220525b9353f2d8e9bd11cfc5f10520c28b8c
genaroibc <bonavitaismael@gmail.com>
2023-03-21 19:53:54 -0300

    [FEAT]: let users add title to their components and move form logic to CodeEditor

--- src/app/editor/components/CodeEditor/CodeEditor.tsx
+++ src/app/editor/components/CodeEditor/CodeEditor.tsx
@@ -1,21 +1,36 @@
 "use client";
 
-import { useState } from "react";
+import { useId, useState } from "react";
 import styles from "./CodeEditor.module.css";
 import Editor from "@monaco-editor/react";
 import { useSupabase } from "@/hooks/useSupabase";
 import Image from "next/image";
+import { Database } from "@/types/db";
 
 type Props = {
-  // eslint-disable-next-line no-unused-vars
-  onSubmit: ({ code }: { code: string }) => void;
+  onSubmit: (
+    // eslint-disable-next-line no-unused-vars
+    data: Database["public"]["Tables"]["components"]["Insert"]
+  ) => void;
 };
 
 export function CodeEditor({ onSubmit }: Props) {
   const { session, supabase } = useSupabase();
+  const componentTitleInputID = useId();
   const [code, setCode] = useState(
     "<h1 class='text-center font-extrabold text-4xl text-blue-400'>Hello world</h1>"
   );
+  const [componentTitle, setComponentTitle] = useState("");
+
+  const handleSubmit = () => {
+    if (!session?.user) return;
+
+    onSubmit({
+      author_username: session.user.user_metadata.user_name,
+      html_code: code,
+      title: componentTitle,
+    });
+  };
 
   return (
     <div className={styles.editorContainer}>
@@ -30,12 +45,38 @@ export function CodeEditor({ onSubmit }: Props) {
       />
 
       {session?.user ? (
-        <button onClick={() => onSubmit({ code })}>Publish component</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={(event) => setComponentTitle(event.target.value)}
+            type="text"
+            name={componentTitleInputID}
+            id={componentTitleInputID}
+            placeholder="my awesome component"
+            required
+            minLength={5}
+            maxLength={40}
+          />
+
+          <label htmlFor={componentTitleInputID}>Component title</label>
+          <button className={styles.publish_btn}>
+            Publish component
+            <Image
+              width={30}
+              height={30}
+              alt="publish component"
+              src="/svg/face-stars.svg"
+            />
+          </button>
+        </form>
       ) : (
         <button
+          className={styles.login_btn}
           onClick={() =>
             supabase.auth.signInWithOAuth({
               provider: "github",
+              options: {
+                redirectTo: window.location.href,
+              },
             })
           }
         >
--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,33 +1,21 @@
 "use client";
 
 import { useSupabase } from "@/hooks/useSupabase";
+import { Database } from "@/types/db";
 import { CodeEditor } from "./components/CodeEditor/CodeEditor";
-import { useState } from "react";
 
 function HomePage() {
-  const { supabase, session } = useSupabase();
-  const [data, setData] = useState<any>();
+  const { supabase } = useSupabase();
 
-  const handleSubmit = async ({ code }: { code: string }) => {
-    if (!session?.user) return;
-
-    await supabase.from("components").insert({
-      author_username: session.user.user_metadata.user_name,
-      html_code: code,
-    });
-  };
-  const handleRequest = async () => {
-    const result = await supabase.from("components").select("*");
-    setData(result);
+  const handleSubmit = async (
+    componentData: Database["public"]["Tables"]["components"]["Insert"]
+  ) => {
+    await supabase.from("components").insert(componentData);
   };
 
   return (
     <div>
       <CodeEditor onSubmit={handleSubmit} />
-      <button onClick={handleRequest}>Get and show supabase data</button>
-      <code>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </code>
     </div>
   );
 }
