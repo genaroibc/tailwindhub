b4c0798e2eea0af0e3c57fa2c5c9151849280b6f
genaroibc <bonavitaismael@gmail.com>
2023-03-22 14:48:32 -0300

    [FEAT]: add Header for mobile

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,19 +1,39 @@
+"use client";
+
 import Link from "next/link";
 import { Login } from "../shared/Login/Login";
 import styles from "./Header.module.css";
+import { useRef } from "react";
 
 export function Header() {
+  const menuRef = useRef<HTMLHeadingElement>(null);
+
+  const toggleMenu = () => {
+    menuRef.current?.classList.toggle(styles.menu_visible);
+  };
+
   return (
-    <header className={styles.header}>
-      <nav className={styles.header__navbar}>
-        <Link className={styles.header__navbar__link} href="/editor">
+    <header className={styles.page_header}>
+      <span className={styles.brand_name}>TailwindHub</span>
+      <nav onClick={toggleMenu} ref={menuRef} className={styles.hamburger_menu}>
+        <Link className={styles.hamburger_menu_link} href="/editor">
           Editor
         </Link>
-        <Link className={styles.header__navbar__link} href="/">
+        <Link className={styles.hamburger_menu_link} href="/">
           Home
         </Link>
       </nav>
       <Login />
+
+      <button
+        onClick={toggleMenu}
+        aria-label="open menu button"
+        className={styles.hamburger_menu_btn}
+      >
+        <span className={styles.hamburger_stick}></span>
+        <span className={styles.hamburger_stick}></span>
+        <span className={styles.hamburger_stick}></span>
+      </button>
     </header>
   );
 }
