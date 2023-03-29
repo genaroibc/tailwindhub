5c26de8768a16246c386a98b10d7d9985b56b362
genaroibc <bonavitaismael@gmail.com>
2023-03-29 15:58:32 -0300

    [DEV]: add types for async operations

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,13 @@ export type SearchFilter =
   | "navbar"
   | "input"
   | "image";
+
+export type KnownError = { ok: false; error: string };
+export type KnownResponse<T> = { ok: true; data: T };
+export type KnownResult<T> = KnownResponse<T> | KnownError;
+
+export type ComponentData = {
+  code: string;
+  title: string;
+  imageDataURL: string;
+};
