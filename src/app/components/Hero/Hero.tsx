5c7c3dd37ad37418be6f06da16cd5a7472a163ab
genaroibc <bonavitaismael@gmail.com>
2023-04-01 15:22:07 -0300

    [FEAT]: re-style Hero

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -4,14 +4,21 @@ import styles from "./Hero.module.css";
 export function Hero() {
   return (
     <div className={styles.hero}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Welcome to TailwindHub</h1>
+      <div className={styles.main}>
+        <h1 className={styles.title}>TailwindHub</h1>
         <p className={styles.subtitle}>
           The best place to share Tailwind components
         </p>
         <Link href="/editor" className={styles.button}>
           Go to editor
         </Link>
+      </div>
+
+      <div className={styles.images}>
+        <img src="https://picsum.photos/200" alt="a login form component" />
+        <img src="https://picsum.photos/200" alt="a user card component" />
+        <img src="https://picsum.photos/200" alt="a fancy button component" />
+        <img src="https://picsum.photos/200" alt="a unordered list component" />
       </div>
     </div>
   );
