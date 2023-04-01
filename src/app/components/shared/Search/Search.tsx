5eeff5c5dd402e916e3338cb4ed90e476376af5f
genaroibc <bonavitaismael@gmail.com>
2023-04-01 19:27:47 -0300

    [FEAT]: remove filters in Search component and style it

--- src/app/components/shared/Search/Search.tsx
+++ src/app/components/shared/Search/Search.tsx
@@ -1,23 +1,28 @@
 "use client";
 
-import { SEARCH_FILTERS } from "@/constants";
+// import { SEARCH_FILTERS } from "@/constants";
+import { SearchIcon } from "../Icons";
 import styles from "./Search.module.css";
 
 export function Search() {
   return (
-    <div className={styles.search_container}>
+    <form className={styles.search_container}>
       <input
         type="text"
-        placeholder="Search for components"
+        placeholder="Search for components..."
         className={styles.search_container__search_input}
       />
-      <select className={styles.filters_list}>
+      <button className={styles.search_container__search_button}>
+        <span>search</span>
+        <SearchIcon width="20" color="#333" />
+      </button>
+      {/* <select className={styles.filters_list}>
         {SEARCH_FILTERS.map(({ title, value }) => (
           <option key={value} value={value}>
             {title}
           </option>
         ))}
-      </select>
-    </div>
+      </select> */}
+    </form>
   );
 }
