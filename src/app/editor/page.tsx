ff685b186ba9149958cb90798576c7d467862bcf
genaroibc <bonavitaismael@gmail.com>
2023-04-02 13:06:29 -0300

    [FEAT]: set max width for editor page content

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from "next";
 import { EditorSection } from "@/app/editor/components/EditorSection/EditorSection";
 import { TailwindScript } from "@/app/components/shared/TailwindScript";
+import styles from "./page.module.css";
 
 export const metadata: Metadata = {
   title: "Editor - TailwindHub",
@@ -9,9 +10,11 @@ export const metadata: Metadata = {
 
 function HomePage() {
   return (
-    <div>
-      <TailwindScript />
-      <EditorSection />
+    <div className={styles.page_wrapper}>
+      <main className={styles.page_wrapper__main}>
+        <TailwindScript />
+        <EditorSection />
+      </main>
     </div>
   );
 }
