254c7de45a1de530c3ff35a69dc876e2ee72cc52
genaroibc <bonavitaismael@gmail.com>
2023-04-02 18:16:35 -0300

    [FEAT]: add emmet support

--- src/app/editor/components/CodeEditor/CodeEditor.tsx
+++ src/app/editor/components/CodeEditor/CodeEditor.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styles from "./CodeEditor.module.css";
 import Editor from "@monaco-editor/react";
 import { DEFAULT_CODE_EDITOR_VALUE } from "@/constants";
+import { emmetHTML } from "emmet-monaco-es";
+import { Loader } from "@/app/components/shared/Loader/Loader";
 
 type Props = {
   codePreviewRef: React.MutableRefObject<null>;
@@ -14,13 +16,20 @@ export function CodeEditor({ codeEditorRef, codePreviewRef }: Props) {
   return (
     <div className={styles.container}>
       <Editor
-        onMount={(editor) => (codeEditorRef.current = editor)}
+        onMount={(editor, monaco) => {
+          codeEditorRef.current = editor;
+          emmetHTML(monaco);
+        }}
         className={styles.editor}
         theme="vs-dark"
         defaultLanguage="html"
         defaultValue={code}
         onChange={(code) => setCode(code ?? "")}
         line={2}
+        loading={<Loader color="var(--primary-color, #fff)" />}
+        options={{
+          minimap: { enabled: false },
+        }}
       />
 
       <div
