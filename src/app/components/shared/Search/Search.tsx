36db7f0c904475b163ab6d23f40071161ff804bf
genaroibc <bonavitaismael@gmail.com>
2023-04-07 16:22:53 -0300

    [FEAT]: style Search component and global buttons and form elements

--- src/app/components/shared/Search/Search.tsx
+++ src/app/components/shared/Search/Search.tsx
@@ -6,14 +6,13 @@ import { Button } from "@/app/components/shared/Button";
 
 export function Search() {
   return (
-    <form className="flex flex-wrap items-center justify-center gap-4 bg-white p-4 max-w-full my-0 mx-auto rounded-lg">
+    <form className="flex flex-wrap items-center justify-center bg-white p-2 max-w-full my-0 mx-auto rounded-lg">
       <input
         type="text"
         placeholder="Search for components..."
-        className="py-2 px-4 rounded-lg text-base min-w-0"
+        className="py-2 px-4 rounded-tl-lg rounded-bl-lg text-base min-w-0"
       />
-      <Button variant="secondary">
-        <span className="hidden sm:block">search</span>
+      <Button variant="secondary" className="rounded-tr-lg rounded-br-lg">
         <SearchIcon width="20" fill="#333" />
       </Button>
       {/* <select className="py-2 px-4 rounded-lg text-base text-dimmed-black bg-secondary-color">
