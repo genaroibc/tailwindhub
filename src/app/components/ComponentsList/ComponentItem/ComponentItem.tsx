3c83367757f8cb66ab65b756a836dfd01c2919e0
genaroibc <bonavitaismael@gmail.com>
2023-04-08 22:31:59 -0300

    [FEAT]: show component tags

--- src/app/components/ComponentsList/ComponentItem/ComponentItem.tsx
+++ src/app/components/ComponentsList/ComponentItem/ComponentItem.tsx
@@ -11,6 +11,7 @@ export function ComponentItem({
   title,
   preview_img,
   author_avatar_url,
+  tags,
 }: Props) {
   return (
     <article
@@ -24,34 +25,42 @@ export function ComponentItem({
           alt={title}
         />
 
-        <footer className="absolute bottom-2 right-2 opacity-0 invisible duration-300 transition-[transform,visibility,opacity] translate-y-2 group-hover:opacity-100 group-hover:translate-y-0 group-hover:visible">
-          <ComponentItemNavBar
-            componentId={id}
-            likes={likes}
-            downloads={0}
-            textToCopy={html_code}
-          />
+        <footer className="bottom-0 w-full absolute opacity-0 duration-300 transition-opacity group-hover:opacity-100 bg-gradient-to-t from-black to-black/0 min-h-[6rem] rounded-bl-2xl rounded-br-2xl flex flex-col justify-end">
+          <div className="opacity-0 invisible duration-300 transition-[transform,visibility,opacity] translate-y-2 flex justify-between items-end gap-2 p-2 group-hover:visible group-hover:opacity-100 group-hover:translate-y-0">
+            <ul className="flex gap-x-1 gap-y-2 items-center justify-between flex-wrap-reverse text-xs">
+              {tags.map((tag) => (
+                <li
+                  key={tag}
+                  className="bg-primary-color py-1 px-2 rounded-full"
+                >
+                  {tag}
+                </li>
+              ))}
+            </ul>
+            <ComponentItemNavBar
+              componentId={id}
+              likes={likes}
+              downloads={0}
+              textToCopy={html_code}
+            />
+          </div>
         </footer>
       </div>
 
       <footer className="flex flex-row gap-2 py-4 px-0 bg-transparent text-primary-color items-center">
-        {/* <pre>{JSON.stringify(tags)}</pre> */}
         <img
-          className="rounded-full max-w-[3rem] aspect-square bg-pink-200 font-normal"
+          className="rounded-full w-full max-w-[2rem] max-h-[2rem] h-full aspect-square bg-pink-200 font-normal"
           src={author_avatar_url}
           alt={`${author_username} avatar`}
           width={30}
           height={30}
         />
 
-        <div className="flex flex-col gap-2 justify-center items-start overflow-hidden">
+        <div className="flex flex-col gap-1 justify-center items-start overflow-hidden">
           <span className="text-base font-semibold w-full overflow-hidden text-ellipsis whitespace-nowrap">
             {title}
           </span>
-          <a
-            className="flex items-center gap-2 text-xs bg-transparent"
-            href={`https://github.com/${author_username}`}
-          >
+          <a className="text-xs" href={`https://github.com/${author_username}`}>
             @{author_username}
           </a>
         </div>
