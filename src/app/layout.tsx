7fdeedf34e1d42844d4c2e300da2d1729a19b5ae
genaroibc <bonavitaismael@gmail.com>
2023-04-23 17:54:29 -0300

    [FEAT]: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Lexend_Deca } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const lexendDeca = Lexend_Deca({
   weight: ["300", "600", "800"],
@@ -13,6 +14,7 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html className={lexendDeca.className} lang="en">
+      <Analytics />
       <body>{children}</body>
     </html>
   );
