77cae0eb4fef462d8a3160378b492cdc53eb095e
genaroibc <bonavitaismael@gmail.com>
2023-04-25 12:49:26 -0300

    [FEAT]: add metadata for editor page

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -3,8 +3,38 @@ import { EditorSection } from "@/app/editor/components/EditorSection";
 import { TailwindScript } from "@/app/components/shared/TailwindScript";
 
 export const metadata: Metadata = {
-  title: "Editor - TailwindHub",
-  description: "Create and publish your own Tailwind components",
+  applicationName: "TailwindHub",
+  colorScheme: "dark",
+  title: "TailwindHub - Editor",
+  description: "Create and share your own Tailwind components",
+  keywords: [
+    "tailwind",
+    "tailwindcss",
+    "components",
+    "ui",
+    "tailwind components",
+    "tailwind playground",
+    "components kit",
+    "components library",
+    "tailwind library",
+    "tailwind kit",
+    "tailwind ui",
+    "tailwind open-source",
+    "tailwind editor",
+  ],
+  openGraph: {
+    title: "TailwindHub - Code editor to create and share Tailwind components",
+    type: "website",
+    description: "Create and share your own Tailwind components",
+    images: "https://tailwindhub.dev/tailwindhub-editor.png",
+    url: "https://tailwindhub.dev/editor",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TailwindHub - Code editor to create and share Tailwind components",
+    description: "Create and share your own Tailwind components",
+    images: ["https://tailwindhub.dev/tailwindhub-editor.png"],
+  },
 };
 
 function HomePage() {
