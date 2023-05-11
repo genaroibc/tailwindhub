5d4014d129d0b2bf7bebcce9294b858cca1e819b
genaroibc <bonavitaismael@gmail.com>
2023-05-11 20:35:46 -0300

    [DEV]: add resizer icons

---
+++ src/app/editor/components/Preview/ResizeIcons.tsx
@@ -0,0 +1,66 @@
+export const IconResizeBottomRight = () => (
+  <svg
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+  >
+    <g strokeWidth="0"></g>
+    <g strokeLinecap="round" strokeLinejoin="round"></g>
+    <g>
+      <path d="M22.354 9.354l-.707-.707-13 13 .707.707zm0 7l-.707-.707-6 6 .707.707z"></path>
+      <path fill="none" d="M0 0h24v24H0z"></path>
+    </g>
+  </svg>
+);
+
+export const IconResizeBottom = () => (
+  <svg
+    viewBox="0 0 16 6"
+    width={16}
+    height={6}
+    fill="none"
+    stroke="currentColor"
+  >
+    <path d="M 0 0.5 H 16 M 0 5.5 H 16" />
+  </svg>
+);
+
+export const IconResizeBottomLeft = () => (
+  <svg
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    transform="matrix(-1, 0, 0, 1, 0, 0)"
+  >
+    <g strokeWidth="0"></g>
+    <g strokeLinecap="round" strokeLinejoin="round"></g>
+    <g>
+      <path d="M22.354 9.354l-.707-.707-13 13 .707.707zm0 7l-.707-.707-6 6 .707.707z"></path>
+      <path fill="none" d="M0 0h24v24H0z"></path>
+    </g>
+  </svg>
+);
+
+export const IconResizeRight = () => (
+  <svg
+    viewBox="0 0 6 16"
+    width={6}
+    height={16}
+    fill="none"
+    stroke="currentColor"
+  >
+    <path d="M 0.5 0 V 16 M 5.5 0 V 16" />
+  </svg>
+);
+
+export const IconResizeLeft = () => (
+  <svg
+    viewBox="0 0 6 16"
+    width={6}
+    height={16}
+    fill="none"
+    stroke="currentColor"
+  >
+    <path d="M 0.5 0 V 16 M 5.5 0 V 16" />
+  </svg>
+);
