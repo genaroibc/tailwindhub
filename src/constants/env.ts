bd2a93f288fbd0f0f7ddc563854a97ee54421e71
genaroibc <bonavitaismael@gmail.com>
2023-06-18 22:26:44 -0300

    [DEV]: throw env variable error only in server side

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -5,11 +5,15 @@ const ENV = {
     process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!,
   NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL!,
   NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY!,
 };
+
+const IS_SERVER = typeof window === "undefined";
 
 Object.entries(ENV).forEach(([key, value]) => {
   if (value === undefined) {
-    throw new Error(`${key} environment variable is not defined`);
+    if (IS_SERVER)
+      throw new Error(`${key} environment variable is not defined`);
   }
 });
 
