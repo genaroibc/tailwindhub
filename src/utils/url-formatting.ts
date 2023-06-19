94d4171ea59f48066751965ae0ce9881db315009
genaroibc <bonavitaismael@gmail.com>
2023-06-19 17:04:26 -0300

    [FIX]: correctly unsluglify string

--- src/utils/url-formatting.ts
+++ src/utils/url-formatting.ts
@@ -1,7 +1,7 @@
 export function sluglify(text: string) {
-  return text.replaceAll(/[\s]/g, "-");
+  return text.replaceAll(" ", "-");
 }
 
 export function unsluglify(text: string) {
-  return text.replaceAll(/[\s]/g, "-");
+  return text.replaceAll("-", " ");
 }
