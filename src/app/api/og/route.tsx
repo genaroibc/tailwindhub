00148ae2ac2c4620ebf2cea3724f66481e9a556f
genaroibc <bonavitaismael@gmail.com>
2023-06-19 14:42:18 -0300

    [FIX]: use img tag instead of background image

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,3 +1,4 @@
+import { BASE_URL } from "@/constants";
 import { ImageResponse } from "next/server";
 
 export const runtime = "edge";
@@ -13,30 +14,33 @@ export async function GET(req: Request) {
   const hasImage = searchParams.has("image");
   const imageURL = hasImage
     ? searchParams.get("image")?.slice(0, 100)
-    : "https://avatars.githubusercontent.com/u/98661193?v=4";
+    : `https://github.com/${username}.png`;
 
   return new ImageResponse(
     (
-      <div
-        tw="h-screen flex items-center pt-28 gap-4 justify-center text-black w-full"
-        style={{
-          backgroundImage: "url(http://localhost:3000/tailwindhub-empty.png)",
-        }}
-      >
+      <article tw="h-screen flex items-center justify-center text-black w-full relative">
         <img
-          tw="w-20 h-20 rounded-full mt-4"
-          width="320"
-          height="320"
-          src={imageURL}
-          alt={`${username}'s profile image`}
+          src={`${BASE_URL}/tailwindhub-empty.png`}
+          alt=""
+          className="absolute inset-0"
         />
-        <div tw="flex flex-col gap3 ml-3 justify-center">
-          <h2 tw="text-3xl sm:text-4xl font-bold tracking-tight text-left ">
-            {username}
-          </h2>
-          <span>tailwindhub.dev/u/{username}</span>
+
+        <div tw="h-screen flex items-center justify-center text-black w-full absolute inset-0 pt-28">
+          <img
+            tw="w-20 h-20 rounded-full mt-4"
+            width="320"
+            height="320"
+            src={imageURL}
+            alt={`${username}'s profile image`}
+          />
+          <div tw="flex flex-col ml-3 justify-center">
+            <h2 tw="text-3xl sm:text-4xl font-bold tracking-tight text-left ">
+              {username}
+            </h2>
+            <span>tailwindhub.dev/u/{username}</span>
+          </div>
         </div>
-      </div>
+      </article>
     ),
     {
       width: 1200,
