2be7aeb0c3f05ee56397babdff94f2d727bb296b
genaroibc <bonavitaismael@gmail.com>
2023-06-18 23:00:01 -0300

    [FEAT]: add dynamic metadata for user page

--- src/app/(with-header)/u/[username]/page.tsx
+++ src/app/(with-header)/u/[username]/page.tsx
@@ -1,8 +1,60 @@
 import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { cookies, headers } from "next/headers";
-import { Database } from "@/types/db";
+import { type Database } from "@/types/db";
 import { ComponentsList } from "../../components/ComponentsList/ComponentsList";
-import { ComponentItem } from "@/types";
+import { type ComponentItem } from "@/types";
+import { type Metadata } from "next";
+
+type GenerateMetadataProps = {
+  params: { username: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({
+  params,
+}: GenerateMetadataProps): Promise<Metadata> {
+  const { username } = params;
+
+  const USER_PROFILE_URL = `https://tailwindhub.dev/u/${username}`;
+  const TITLE = `${username} - TailwindHub`;
+  const DESCRIPTION = `Visit ${username} profile on TailwindHub`;
+
+  return {
+    metadataBase: new URL(USER_PROFILE_URL),
+    applicationName: "TailwindHub",
+    colorScheme: "dark",
+    title: TITLE,
+    description: DESCRIPTION,
+    keywords: [
+      "tailwind",
+      "tailwindcss",
+      "components",
+      "ui",
+      "tailwind components",
+      "tailwind playground",
+      "components kit",
+      "components library",
+      "tailwind library",
+      "tailwind kit",
+      "tailwind ui",
+      "tailwind open-source",
+      "tailwind editor",
+    ],
+    openGraph: {
+      title: TITLE,
+      type: "website",
+      description: DESCRIPTION,
+      images: "https://tailwindhub.dev/tailwindhub.png",
+      url: USER_PROFILE_URL,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: TITLE,
+      description: DESCRIPTION,
+      images: ["https://tailwindhub.dev/tailwindhub.png"],
+    },
+  };
+}
 
 export const revalidate = 60;
 
