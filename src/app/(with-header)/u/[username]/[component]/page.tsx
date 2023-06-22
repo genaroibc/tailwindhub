0dc26dfa9231d87842c5dfe4aafb43d9fdcf4f7e
genaroibc <bonavitaismael@gmail.com>
2023-06-22 14:18:42 -0300

    [FEAT]: add og image for component page

--- src/app/(with-header)/u/[username]/[component]/page.tsx
+++ src/app/(with-header)/u/[username]/[component]/page.tsx
@@ -4,6 +4,8 @@ import { type Database } from "@/types/db";
 import { type ComponentItem as TComponentItem } from "@/types";
 import { unsluglify } from "@/utils/url-formatting";
 import { ComponentPage } from "./components/ComponentPage";
+import { Metadata } from "next";
+import { BASE_URL } from "@/constants";
 
 type PageProps = {
   params: { username: string; component: string };
@@ -28,4 +30,62 @@ export default async function Page({ params }: PageProps) {
       )}
     </main>
   );
+}
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const { username, component } = params;
+
+  const COMPONENT_PAGE_URL = `${BASE_URL}/u/${username}/${component}`;
+  const TITLE = `${component} - TailwindHub`;
+  const DESCRIPTION = `${component} by ${username}`;
+
+  const ogUrl = new URL(`${BASE_URL}/api/og`);
+  ogUrl.searchParams.set("username", username || "");
+  ogUrl.searchParams.set("component", component || "");
+  ogUrl.searchParams.set("image", `https://github.com/${username}.png`);
+
+  return {
+    title: TITLE,
+    description: DESCRIPTION,
+    openGraph: {
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: username,
+        },
+      ],
+      title: TITLE,
+      type: "article",
+      description: DESCRIPTION,
+      url: COMPONENT_PAGE_URL,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: TITLE,
+      description: DESCRIPTION,
+      images: [ogUrl.toString()],
+    },
+    metadataBase: new URL(COMPONENT_PAGE_URL),
+    applicationName: "TailwindHub",
+    colorScheme: "dark",
+    keywords: [
+      "tailwind",
+      "tailwindcss",
+      "components",
+      "ui",
+      "tailwind components",
+      "tailwind playground",
+      "components kit",
+      "components library",
+      "tailwind library",
+      "tailwind kit",
+      "tailwind ui",
+      "tailwind open-source",
+      "tailwind editor",
+    ],
+  };
 }
