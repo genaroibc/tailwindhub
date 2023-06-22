0b3babe10f2d85d4fec8dff53251e1a95f966214
genaroibc <bonavitaismael@gmail.com>
2023-06-22 14:16:21 -0300

    [DEV]: add types for supabase singleton

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import ENV from "@/constants/env";
+import { Database } from "@/types/db";
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   ENV.NEXT_PUBLIC_SUPABASE_URL,
   ENV.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
