4b67054275c1a10ae725d967b16bfacae3d36e28
genaroibc <bonavitaismael@gmail.com>
2023-06-25 12:01:32 -0300

    [FIX]: properly encode emojis

--- src/utils/encode-decode-url.ts
+++ src/utils/encode-decode-url.ts
@@ -1,7 +1,9 @@
+import { encode as encodeBase64, decode as decodeBase64 } from "js-base64";
+
 export function encode(string: string) {
-  return globalThis.encodeURIComponent(globalThis.btoa(string));
+  return globalThis.encodeURIComponent(encodeBase64(string));
 }
 
 export function decode(string: string) {
-  return globalThis.atob(globalThis.decodeURIComponent(string));
+  return decodeBase64(globalThis.decodeURIComponent(string));
 }
