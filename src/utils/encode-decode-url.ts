9bc22e4827319d4830609dd1063e94c4cf13b280
genaroibc <bonavitaismael@gmail.com>
2023-06-25 16:25:04 -0300

    [FIX]: prevent encoding nullish values

--- src/utils/encode-decode-url.ts
+++ src/utils/encode-decode-url.ts
@@ -1,9 +1,9 @@
 import { encode as encodeBase64, decode as decodeBase64 } from "js-base64";
 
 export function encode(string: string) {
-  return globalThis.encodeURIComponent(encodeBase64(string));
+  return globalThis.encodeURIComponent(encodeBase64(string ?? ""));
 }
 
 export function decode(string: string) {
-  return decodeBase64(globalThis.decodeURIComponent(string));
+  return decodeBase64(globalThis.decodeURIComponent(string ?? ""));
 }
