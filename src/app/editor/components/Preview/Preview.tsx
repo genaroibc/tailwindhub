a7ee049d0d20f1aa2064a3271bc5702ce91595ca
genaroibc <bonavitaismael@gmail.com>
2023-07-05 14:35:00 -0300

    [FEAT]: render iframe when is resizable

--- src/app/editor/components/Preview/Preview.tsx
+++ src/app/editor/components/Preview/Preview.tsx
@@ -227,6 +227,7 @@ export const Preview = ({ code, isResizable, codePreviewRef }: Props) => {
             <IconResizeLeft />
           </span>
         )}
+
         <div
           className={`relative ${
             isResizable
@@ -248,31 +249,44 @@ export const Preview = ({ code, isResizable, codePreviewRef }: Props) => {
               : {}
           }
         >
-          <div
-            ref={codePreviewRef}
-            dangerouslySetInnerHTML={{ __html: code }}
-            style={
-              isResizable
-                ? {
-                    width: constrainedResponsiveSize.width,
-                    height: constrainedResponsiveSize.height,
-                    marginLeft:
-                      (constrainedResponsiveSize.width -
-                        Math.round(
-                          constrainedResponsiveSize.width *
-                            constrainedResponsiveSize.zoom
-                        )) /
-                      -2,
-                    transformOrigin: "top",
-                    transform: `scale(${constrainedResponsiveSize.zoom})`,
-                  }
-                : {}
-            }
-            className={`flex min-h-[400px] items-center p-4 justify-center w-full relative h-full overflow-auto bg-white text-dimmed-black ![&_img]:inline-block inset-0 ${
-              resizing ? "pointer-events-none select-none" : ""
-            }`}
-          />
+          {isResizable ? (
+            <iframe
+              className={resizing ? "pointer-events-none select-none" : ""}
+              style={{
+                width: constrainedResponsiveSize.width,
+                height: constrainedResponsiveSize.height,
+                marginLeft:
+                  (constrainedResponsiveSize.width -
+                    Math.round(
+                      constrainedResponsiveSize.width *
+                        constrainedResponsiveSize.zoom
+                    )) /
+                  -2,
+                transformOrigin: "top",
+                transform: `scale(${constrainedResponsiveSize.zoom})`,
+              }}
+              srcDoc={`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Code Preview</title>
+  <script src="/tailwind-3.2.6.min.js"></script>
+</head>
+<body class="flex min-h-[400px] items-center p-4 justify-center w-full relative h-full overflow-auto bg-white text-dimmed-black ![&_img]:inline-block inset-0">
+  ${code}
+</body>
+</html>`}
+            />
+          ) : (
+            <div
+              ref={codePreviewRef}
+              dangerouslySetInnerHTML={{ __html: code }}
+              className="flex min-h-[400px] items-center p-4 justify-center w-full relative h-full overflow-auto bg-white text-dimmed-black ![&_img]:inline-block inset-0"
+            />
+          )}
         </div>
+
         {isResizable && (
           <>
             <Resizer
